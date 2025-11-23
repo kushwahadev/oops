2c6582d3c9563777bbf412cf2f2bf65a74b5e30b
Dev Kushwaha <devkushwaha@Devs-MacBook-Air.local>
2025-11-24 03:04:51 +0530

    cpp encapsulation

---
+++ oops_cpp/01-encapsulation/encap.cpp
@@ -0,0 +1,25 @@
+// encapsulation
+// Design a simple class called BankAccount that supports the following features:
+
+// Each account has a unique account number, account holder name, and balance.
+
+// Provide functions to:
+
+// deposit(amount)
+
+// withdraw(amount) (only if sufficient balance)
+
+// display() — prints account details.
+
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+private:
+    string accountNumber;
+    string clientName;
+    double balance;
+
+public:
+};
---
+++ oops_cpp/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+void def_fun()
+{
+}
+int main()
+{
+    int t;
+    cout << "enter a number : ";
+    cin >> t;
+    cout << t << endl;
+    string s;
+    cout << "enter your name: ";
+    cin >> s;
+    cout << s << endl;
+    return 0;
+}
