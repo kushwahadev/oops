539daf7d861c86175da811f05f3cf3831c3f8015
Dev Kushwaha <devkushwaha@Devs-MacBook-Air.local>
2025-11-27 23:34:24 +0530

    cpp encapsulation

--- oops_cpp/01-encapsulation/encap.cpp
+++ oops_cpp/01-encapsulation/encap.cpp
@@ -1,15 +1,4 @@
 // encapsulation
-// Design a simple class called BankAccount that supports the following features:
-
-// Each account has a unique account number, account holder name, and balance.
-
-// Provide functions to:
-
-// deposit(amount)
-
-// withdraw(amount) (only if sufficient balance)
-
-// display() — prints account details.
 
 #include <iostream>
 using namespace std;
@@ -22,9 +11,53 @@ private:
     double balance;
 
 public:
+    BankAccount(string accNum, string name, double currBala)
+    {
+        accountNumber = accNum;
+        clientName = name;
+        balance = currBala;
+    }
+
+    // check balance
+
+    double checkBalance()
+    {
+        return balance;
+    }
+
+    // deposit money
+    void deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            cout << "Deposited: $" << amount << endl;
+        }
+        else
+        {
+            cout << "Invalid deposit amount." << endl;
+        }
+    }
+
+    // withdraw money
+    void withdraw(double amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+            cout << "Withdrew: $" << amount << endl;
+        }
+    }
 };
 
 int main()
 {
+
+    BankAccount acc1("SBI1234", "John Snow", 5000.0);
+
+    cout << acc1.checkBalance() << endl;
+    acc1.deposit(1500.0);
+    acc1.withdraw(2000.0);
+    cout << acc1.checkBalance() << endl;
     return 0;
 }
